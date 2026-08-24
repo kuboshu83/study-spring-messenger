6c813be6b5d929ab1295484188e925101b9fb920
kuboshu83 <dot.sohkubo@gmail.com>
2026-08-24 11:05:10 +0900

    mailipt導入

--- app/src/main/kotlin/org/example/notification/domain/service/MessageNotificator.kt
+++ app/src/main/kotlin/org/example/notification/domain/service/MessageNotificator.kt
@@ -1,15 +1,39 @@
 package org.example.notification.domain.service
 
 import org.example.notification.domain.model.Message
-import org.springframework.stereotype.Component
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
 
 interface MessageNotificator {
     fun notify(message: Message)
 }
 
-@Component
-class ConsoleMessageNotificator : MessageNotificator {
+class MailMessageNotificator(private val from: String, private val mailSender: JavaMailSender) : MessageNotificator {
     override fun notify(message: Message) {
-        println(message)
+        val simpleMailMessage = SimpleMailMessage()
+        simpleMailMessage.subject = message.title.value
+        val destinations = message.destinations.toSet().map { it.value }
+        simpleMailMessage.setTo(*destinations.toTypedArray())
+        simpleMailMessage.from = from
+        simpleMailMessage.text = message.body.value
+        mailSender.send(simpleMailMessage)
     }
 }
+
+@Configuration
+class NotificationConfig {
+    @Bean
+    fun mailMessageNotificator(mailSender: JavaMailSender): MessageNotificator {
+        val from = "from@example.org"
+        return MailMessageNotificator(from, mailSender)
+    }
+}
+
+//@Component
+//class ConsoleMessageNotificator : MessageNotificator {
+//    override fun notify(message: Message) {
+//        println(message)
+//    }
+//}
